94cd9a325aff4da4af75b2d28908478cae1b4820
najuwamr <najuwamaulidarhm@gmail.com>
2025-05-01 13:35:07 +0700

    commit

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function showLoginForm()
+    public function index()
     {
         return view('login');
     }
